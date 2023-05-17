d6efbed25048a3c2abb7c98f7935c6eacbd120a1
Asad Rizvi <asadrzv@outlook.com>
2023-05-16 23:15:07 -0400

    Minor fixes

--- DigimonFieldGuide/New Group/DigimonListView.swift
+++ DigimonFieldGuide/New Group/DigimonListView.swift
@@ -10,6 +10,10 @@ import SwiftUI
 struct DigimonListView: View {
     @ObservedObject var digimonViewModel = DigimonViewModel(digimonManager: DigimonManager())
     
+    init() {
+        digimonViewModel.getAllDigimon()
+    }
+    
     var body: some View {
         NavigationStack {
             // List of all Digimon cells (name/image) that link to details when tapped
@@ -18,10 +22,7 @@ struct DigimonListView: View {
                     DigimonCellView(digimon: digimon)
                 }
             }
-            // Get all Digimon and store in view model
-            .onAppear {
-                digimonViewModel.getAllDigimon()
-            }
+            
             // Search bar for user to filter Digimon by name
             .searchable(text: $digimonViewModel.searchText)
             // Animation for searching so Digimon don't simply disapear
